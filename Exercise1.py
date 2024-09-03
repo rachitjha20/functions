4b658b86205f66e71400a69d01a849c1ee9d2a33
rachitjha20 <rachitjha20@gmail.com>
2024-09-03 22:54:33 +0530

    new exercise begin

---
+++ Exercise1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Create a function in Python
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+
+
+
+def argu(name, age):
+    print(name, age)
+    
+argu("Rachit", 24)
+    
